d1d56c0f701ab382004950c25326db7c270527fb
tchfer <tchfernando@gmail.com>
2023-06-12 01:23:36 -0300

    Create Add thought component and template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AddThoughtComponent } from './components/thoughts/add-thought/add-thought.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AddThoughtComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/thoughts/add-thought/add-thought.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-thought',
+  templateUrl: './add-thought.component.html',
+  styleUrls: ['./add-thought.component.css']
+})
+export class AddThoughtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
