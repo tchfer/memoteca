367ee90882ecf4e7217fc888e771c954da3394ad
tchfer <tchfernando@gmail.com>
2023-06-15 00:49:54 -0300

    Add property binding

--- src/app/components/thoughts/add-thought/add-thought.component.ts
+++ src/app/components/thoughts/add-thought/add-thought.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddThoughtComponent implements OnInit {
 
+  thought = {
+    id: '1',
+    content: 'Aprendendo Angular',
+    author: 'Dev',
+    model: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
