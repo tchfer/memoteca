194f2ee1fa16636093d172b93ab39f26e78a35af
tchfer <tchfernando@gmail.com>
2023-06-15 01:02:59 -0300

    Add FormsModule for ngModel directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AddThoughtComponent } from './components/thoughts/add-thought/add-thought.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AddThoughtComponent } from './components/thoughts/add-thought/add-thoug
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
